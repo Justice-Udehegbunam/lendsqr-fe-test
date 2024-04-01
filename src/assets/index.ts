4c85b692c4ced17961057a668e34856f82fca4e4
Justice-Udehegbunam <justicefavour491@gmail.com>
2024-04-01 11:34:17 +0100

    Added images for the nav bar

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,16 @@
 import logo from "./svg/logo.svg";
 import loginIllustration from "./svg/pablo-sign-in 1.svg";
 
-export { logo, loginIllustration };
+import navSearchIcon from "./svg/navSearchSvg.svg";
+import navAvatar from "./img/image 4.png";
+import navNotificationBell from "./svg/navNotification.svg";
+import navArrowDown from "./svg/navArrowDown.svg";
+
+export {
+  logo,
+  loginIllustration,
+  navSearchIcon,
+  navAvatar,
+  navNotificationBell,
+  navArrowDown,
+};
